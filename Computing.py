555d60918056b7c715b701a2f6459469dd28ced1
AlKurpiakov <s23380350@unn.ru>
2025-11-11 22:26:24 +0300

    Changes to be committed: 	modified:   Computing.py

--- Computing.py
+++ Computing.py
@@ -1,23 +1,54 @@
 import numpy as np
 import scipy
+import accessify
 
 class Computing:
     def __init__(self, matirx):
-        self._params = matirx
+        self.__params = matirx
     
+    @accessify.private
+    def FindMinInColumnWhithExclitedRows(self, column_id, excluded_rows):
+        matrix = np.copy(self.__params)
+        col = matrix[:, column_id]
+
+        mask = np.ones(len(col), bool)
+        excluded_indices = [int(idx) for idx in excluded_rows]
+        mask[excluded_indices] = False
+
+        filtered_columns = col[mask]
+
+        min_val = np.min(filtered_columns)
+
+        avel_min = np.where(mask)[0]
+        min_filt_id = np.argmin(filtered_columns)
+        mat_min_id = avel_min[min_filt_id]
+
+        return {min_val, mat_min_id}
+
     def HungarianMinimum(self):
-        row_ind, col_ind = scipy.optimize.linear_sum_assignment(self._params)
-        cost = self._params[row_ind, col_ind].sum()
+        row_ind, col_ind = scipy.optimize.linear_sum_assignment(self.__params)
+        cost = self.__params[row_ind, col_ind].sum()
         return cost
     
     def HungarianMaximum(self):
-            matrix = -self._params.copy()
+            matrix = -self.__params.copy()
             row_ind, col_ind = scipy.optimize.linear_sum_assignment(matrix)
-            cost = self._params[row_ind, col_ind].sum()
+            cost = self.__params[row_ind, col_ind].sum()
             return cost
 
-    def Method_2(self):
-        return 0
+    
+    def ThriftyMethod(self):
+        cost = 0
+        shapes = self.__params.shape
+        assigned_rows = set()
+
+        for i in range(shapes[0]):
+            min_val, row = self.FindMinInColumnWhithExclitedRows(i, assigned_rows)
+            cost += min_val
+            assigned_rows.add(row)
+            
+
+        return cost
     
     # ...
     
