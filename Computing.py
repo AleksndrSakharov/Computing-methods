e3f87f3f4b393a13886ea80917be81f94972c125
AlKurpiakov <s23380350@unn.ru>
2025-11-11 22:13:42 +0300

    realized GreedyMethod

--- Computing.py
+++ Computing.py
@@ -1,23 +1,51 @@
 import numpy as np
 import scipy
+import accessify
 
 class Computing:
     def __init__(self, matirx):
-        self._params = matirx
+        self.__params = matirx
     
+    def findMaxInColumnWhithExclitedRows(self, column_id, excluded_rows):
+        matrix = np.copy(self.__params)
+        col = matrix[:, column_id]
+
+        mask = np.ones(len(col), bool)
+        mask[excluded_rows] = False
+
+        filtered_columns = col[mask]
+
+        max_val = np.max(filtered_columns)
+
+        avel_max = np.where(mask)[0]
+        max_filt_id = np.argmax(filtered_columns)
+        mat_max_id = avel_max[max_filt_id]
+
+        return {max_val, mat_max_id}
+
+
     def HungarianMinimum(self):
-        row_ind, col_ind = scipy.optimize.linear_sum_assignment(self._params)
-        cost = self._params[row_ind, col_ind].sum()
+        row_ind, col_ind = scipy.optimize.linear_sum_assignment(self.__params)
+        cost = self.__params[row_ind, col_ind].sum()
         return cost
     
     def HungarianMaximum(self):
-            matrix = -self._params.copy()
+            matrix = -self.__params.copy()
             row_ind, col_ind = scipy.optimize.linear_sum_assignment(matrix)
-            cost = self._params[row_ind, col_ind].sum()
+            cost = self.__params[row_ind, col_ind].sum()
             return cost
 
-    def Method_2(self):
-        return 0
+    def GreedyMethod(self):
+        cost = 0
+        shapes = self.__params.shape
+        assigned_rows = set()
+
+        for i in range(shapes[1]):
+            max_val, row = findMaxInColumnWhithExclitedRows(i, assigned_rows)
+            if row != -1:
+                cost += 1
+
+        return cost
     
     # ...
     
